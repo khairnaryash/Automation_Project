86aa48f00c08d77a0d13f6679951aee3055addad
khairnaryash <khairnar.yash@gmail.com>
2021-07-19 16:46:56 +0530

    fixed issue with webdriver manager

--- src/main/java/com/automation/utils/SeleniumDriverManager.java
+++ src/main/java/com/automation/utils/SeleniumDriverManager.java
@@ -1,16 +1,12 @@
 package com.automation.utils;
 
-import com.automation.Exceptions.InvalidValueException;
 import com.automation.enums.SupportedBrowser;
 import com.automation.reporter.Log;
 import com.automation.reporter.SeleniumEventListner;
-import io.github.bonigarcia.wdm.BrowserManager;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.edge.EdgeDriver;
-import org.openqa.selenium.edge.EdgeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.events.EventFiringWebDriver;
 
@@ -21,10 +17,9 @@ import java.nio.file.Path;
 import java.nio.file.StandardCopyOption;
 import java.util.HashMap;
 
-public class SeleniumDriverManager extends WebDriverManager {
+public class SeleniumDriverManager {
 
 
-    private static HashMap<String, WebDriver> webDriver = new HashMap<>();
     private static ThreadLocal<WebDriver> webDriverThreadLocal = new ThreadLocal<>();
 
     public static WebDriver getWebDriver() {
@@ -43,17 +38,13 @@ public class SeleniumDriverManager extends WebDriverManager {
 
         Log.debug("Initializing Browser : " + browser.name());
 
-//        System.setProperty("wdm.targetPath", "webdrivers");
-        Log.debug("instance : "+Thread.currentThread().getName());
+        System.setProperty("wdm.targetPath", "target/webdrivers");
+
+        Log.debug("instance : " + Thread.currentThread().getName());
         switch (browser) {
 
             case CHROME: {
-                BrowserManager manager = WebDriverManager.getInstance(ChromeDriver.class).version(version).arch32();
-                manager.setup();
-                String browserPath = manager.getBinaryPath();
-                Log.debug("Driver path : " + browserPath);
-
-                System.setProperty("webdriver.chrome.driver", browserPath);
+                WebDriverManager.chromedriver().setup();
 
                 HashMap<String, Object> chromePrefs = new HashMap<String, Object>();
 //		chromePrefs.put("download.default_directory", DOWNLOAD_DIR);
@@ -66,11 +57,8 @@ public class SeleniumDriverManager extends WebDriverManager {
                 break;
             }
             case CHROME_HEADLESS: {
-                BrowserManager manager = WebDriverManager.getInstance(ChromeDriver.class).version(version).arch32();
-                manager.setup();
-                String browserPath = manager.getBinaryPath();
-                Log.debug("Driver path : " + browserPath);
-                System.setProperty("webdriver.chrome.driver", browserPath);
+
+                WebDriverManager.chromedriver().setup();
 
                 HashMap<String, Object> chromePrefs = new HashMap<String, Object>();
 //		chromePrefs.put("download.default_directory", DOWNLOAD_DIR);
@@ -88,13 +76,8 @@ public class SeleniumDriverManager extends WebDriverManager {
                 break;
             }
             case FIREFOX: {
-                BrowserManager manager = WebDriverManager.getInstance(FirefoxDriver.class).version(version).arch32();
-                manager.setup();
-                String browserPath = manager.getBinaryPath();
-                Log.debug("Driver path : " + browserPath);
-                System.setProperty("webdriver.gecko.driver", browserPath); // set geckodriver.exe
 
-//			DesiredCapabilities capabilities = DesiredCapabilities.firefox();
+                WebDriverManager.firefoxdriver().version(version).setup();
 
                 // initialize FireFox driver
                 setWebDriver(new FirefoxDriver());
@@ -134,46 +117,6 @@ public class SeleniumDriverManager extends WebDriverManager {
                 System.out.println("Unable to create directories: [" + targetDirPath + "].");
             }
         }
-    }
-
-    public static void main(String[] args) throws Exception {
-
-        String version = "91";
-//        String target = "./Driver/chrome/" + version + "/chromedriver.exe";
-        System.setProperty("wdm.targetPath", "./target/webdrivers");
-
-        BrowserManager manager = WebDriverManager.getInstance(ChromeDriver.class).version(version).arch32();
-        manager.setup();
-        String browserPath = manager.getBinaryPath();
-
-//        moveWithPruneEmptydirectories(browserPath, target);
-
-        System.setProperty("webdriver.chrome.driver", browserPath);
-
-        HashMap<String, Object> chromePrefs = new HashMap<String, Object>();
-//		chromePrefs.put("download.default_directory", DOWNLOAD_DIR);
-        chromePrefs.put("profile.content_settings.pattern_pairs.*.multiple-automatic-downloads", 1);
-        ChromeOptions options = new ChromeOptions();
-        options.setExperimentalOption("prefs", chromePrefs);
-        options.addArguments("--test-type");
-        options.addArguments("--start-maximized");
-        options.addArguments("--headless");
-        options.addArguments("--disable-gpu");
-        WebDriver driver = new ChromeDriver(options);
-        WebDriver driver2 = new ChromeDriver(options);
-
-        driver.get("https://en.wikipedia.org/wiki/Main_Page");
-        driver2.get("https://www.google.com");
-
-        Thread.sleep(2000);
-
-        System.out.println(driver.getTitle());
-        System.out.println(driver2.getTitle());
-        System.out.println("Session id 1: " + ((ChromeDriver) driver).getSessionId());
-        System.out.println("Session id 2: " + ((ChromeDriver) driver2).getSessionId());
-
-        driver.close();
-        driver2.close();
     }
 
 }
